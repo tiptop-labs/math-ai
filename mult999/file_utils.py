f6c14b4f0e5ab2a403d3e1052ed1ed51df544b63
TipTop Labs <office@tiptop-labs.com>
2017-12-25 17:35:07 +0100

    bug fix

--- mult999/file_utils.py
+++ mult999/file_utils.py
@@ -43,6 +43,6 @@ def gs_upload(local_filename, filename, mode, content_type = None):
         bucket = client.get_bucket(bucket_name) 
         blob = google.cloud.storage.Blob(path.lstrip("/"), bucket) 
         with open(local_filename, 'rb') as file: 
-            blob.upload_from_file(file, content_type) 
+            blob.upload_from_file(file, content_type = content_type) 
         print("removing {0}".format(local_filename))
         os.remove(local_filename)
